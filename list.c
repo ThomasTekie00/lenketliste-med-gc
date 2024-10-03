9627b63394e8718a2f5820d5718e9433afcd1f39
Thomastekie00 <thtek2805@uit.no>
2024-10-03 14:55:58 +0200

    Fullført list.c

--- list.c
+++ list.c
@@ -15,7 +15,7 @@ typedef struct node    //Slipper å kalle på struct hvergang og gir kallenavn t
 typedef struct list
 {
     node_t *head   //Pointer til første noden i listen
-}list_t;           //Nye navnet på struct listen
+} list_t;           //Nye navnet på struct listen
 
 //funksjon for å lage en ny liste
 list_t *list_create(void)
@@ -114,30 +114,35 @@ void list_remove(list_t *list, void *item)
 
 int list_size(list_t *list)
 {
-    if (list == NULL || list -> head == NULL)
+    if (list == NULL || list -> head == NULL) //Sjekker om listen er NULL eller tom
     {
         return 0;
     }
-    int teller = 0;
-    node_t *current = list -> head;
+    int teller = 0;                          //Teller for å finne antall noder
+    node_t *current = list -> head;          //Starter med første node
 
-    while (current != NULL)
+    while (current != NULL)                  //Går gjennom listen av noder
     {
-        teller++;
-        current = current -> next;
+        teller++;                            //Øker teller fpor hver node
+        current = current -> next;           //Fortsetter til neste node
     }
     
-    return teller;
-}
+    return teller;                           //Returnere antall noder
+} 
 
+
+/*Pointer til lsiten som iterer over */
 typedef struct list_iterator{
-    list_t* list;
-    struct node* current;
+    list_t* list;                       //Pointer til listen som itereres over
+    struct node* current;               //Pointer til noden som pointes på i listen som itereren pointer på
 } list_iterator_t;
 
-list_iterator_t *list_createiterator(list_t *list)
+
+
+
+list_iterator_t *list_createiterator(list_t *list)     //Lager iterator
 {
-    if (list == NULL || list ->head == NULL)
+    if (list == NULL || list ->head == NULL)           //Sjekker om listen er tom elelr NULL
     {
         return NULL;
     }
@@ -147,44 +152,102 @@ list_iterator_t *list_createiterator(list_t *list)
     {
         return NULL;
     }
-    iter -> list = list;
-    iter -> current = list -> head;
-    return iter;
+    iter -> list = list;              //Kobler iteratoren til listen
+    iter -> current = list -> head;   //Starter med første node i listen
+    return iter;                      //Returnere iteratoren
 
 
 }
 
+
+
 void list_destroyiterator(list_iterator_t *iter)
 {
-    if(iter != NULL)
+    if(iter != NULL)         //Sjekker om iteratoren ikke er NULL
     {
-        free(iter);
-    }
+        free(iter);          //Slipper minnet til iteratoren
+    } 
 }
 
 void *list_next(list_iterator_t *iter)
 {
     if (iter == NULL || iter -> current == NULL)
     {
-        return NULL;
+        return NULL;                   //Returnere NULL hvis det er siste node eller om iteratoren er NULL
     }
-    void *data = iter -> current -> data;
+    /*Void så datatypen kan være hva som helst*/
+    void *data = iter -> current -> data;      //Henter dataen fra noden som pointes på
 
-    iter -> current = iter -> current -> next;
+    iter -> current = iter -> current -> next;  //Går videre til neste node
 
     return data;
 }
 
 void list_resetiterator(list_iterator_t *iter)
 {
-    if (iter != NULL && iter -> list != NULL)
+    if (iter != NULL && iter -> list != NULL)     //Sjekker om iteratoren og listen ikke er NULL, begge må være gyldige for å resette
     {
-        iter -> current = iter -> list -> head;
+        iter -> current = iter -> list -> head;  //Går tilbake til første node ved å sette noden som pointes på til første node
     }
 
     
 }
 
 
+/*Main funksjon for å teste linked list implementasjonen og iterator funksjonene. Viser oppretting av en listen og ved tilegging av elementer, og bruke iteratoren til å gå gjennom listen, fjerne elementer og til slutt frigjøre minnet*/
+int main()
+{
+    list_t *my_list = list_create(); //Lager ny liste
 
-// implement the functions from list.h here
+/*Legger til elementer for listen*/
+    list_addfirst(my_list, "Tredje node");
+    list_addfirst(my_list, "Andre node");
+    list_addfirst(my_list, "Første node");
+
+    /*Siste element på slutten av listen ved addlast*/
+    list_addlast(my_list,"Fjerde node");
+
+    /*Skriver ut innholdet med iteratoren*/
+    printf("Innholdet i listen: \n");
+    list_iterator_t *iter = list_createiterator(my_list);
+    void *data;   //Dataen som er i noden, blir ved void (Valgfri datatype)
+
+    while ((data = list_next(iter)) != NULL)
+    {
+        printf("%s -> ", (char* )data); // Bytter type fra void til char* ved hjelp av linje 213
+    } 
+    printf("NULL\n");
+
+
+    /*List remove for å fjerne element fra listen*/
+    list_remove(my_list, "Tredje node"); //Bruke tredje node som et eksempel
+
+
+
+    /*Skrive ut listen på nytt etter tredje node er fjernet*/
+    printf("Innhold i listen etter fjerning av 'Tredje node': \n");
+    iter = list_createiterator(my_list); //Ny iterator for listen etter fjerning  
+    
+    while ((data = list_next(iter)) != NULL)
+    {
+        printf("%s -> ", (char* )data); //Bytter type, samme som linje 217
+    }
+    printf("NULL\n");
+    
+
+
+    /*Skrive ut størrelsen for listen*/
+    printf("Listen har %d elementer.\n", list_size(my_list)); //Skriver ut antall elementer i listen
+
+    /*Resetter iteratoren og skriver ut første element igjen*/
+    list_resetiterator(iter);
+    printf("Etter reset: Så nå er første element i listen: %s\n", (char *)list_next(iter));
+
+    /*Blir kvitt minnet for iteratoren og listen*/
+    list_destroyiterator(iter);
+    list_destroy(my_list);
+
+    return 0;
+    
+
+}
